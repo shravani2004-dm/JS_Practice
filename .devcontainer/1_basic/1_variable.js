4d0a8f57e10ca48607c98e60696f19799b6c4617
Shravani Deepak More <shravaniedmore@gmail.com>
2025-09-18 17:47:08 +0000

    var done

---
+++ .devcontainer/1_basic/1_variable.js
@@ -0,0 +1,6 @@
+const accountId=1432;
+let accountEmail="shrau@gmail.com";
+var accountpass="2003";
+accountcity="nashik";
+console.log(accountId)
+console.table([accountId,accountEmail,accountpass,accountcity]);
