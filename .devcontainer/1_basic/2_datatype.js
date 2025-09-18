6c8ea2f626bbca43d7e017e275dc7c79626c0351
Shravani Deepak More <shravaniedmore@gmail.com>
2025-09-18 19:07:55 +0000

    dt done

---
+++ .devcontainer/1_basic/2_datatype.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert(3+3);// we are using node js so a;ert not work we are not in browser
+let name="shravani";
+let age=21;
+let isApproved=true;
+//data types
+//number=>both int and decimal
+//bigint
+//boolean=>true /false 
+//null=>empty/stand alone value
+//undefined=>value not assigned
+//string=>text
+//symbol=>unique identifier
+//object=> key value pairs
+console.log(typeof age);
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
+console.log()
